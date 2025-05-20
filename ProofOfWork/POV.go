b010b7d69cba2a9ebcee7b962e240ee900f1c818
Om Singh <omsingh.ailearn@gmail.com>
2025-05-20 06:59:11 +0530

    POV

--- ProofOfWork/POV.go
+++ ProofOfWork/POV.go
@@ -1,10 +1,16 @@
-package proof-of-work
+package ProofOfWork
 import (
 	"github.com/OmSingh2003/blockchain-go/blocks"
 )
-
-
 const targetBits = 24 
 type ProofOFWork struct {
-	block 
+	block      *blocks.block
+	tartget    *big.int
 }
+func NewProofOfWork(b *blocks.Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	
+	pow :=&ProofOfWork{b,target}
+}
+
