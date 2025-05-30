44e51d7164f1830ff7790e26eb05482c78f8e150
Om Singh <omsingh.ailearn@gmail.com>
2025-05-30 07:30:10 +0530

    working on implementing addresses for user authentication : wallet structure

---
+++ wallet/wallet.go
@@ -0,0 +1,25 @@
+package wallet
+import (
+
+)
+type Wallet struct {
+	PrivateKey   ecdsa.PrivateKey
+	PublicKey    []byte
+}
+type Wallets struct  {
+	Wallets map[string] *Wallet
+}
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{private,public}
+
+	return &wallet
+}
+
+func newKeyPair() (ecdsa.PrivateKey,[]byte) {
+	curve := elliptic.P256()
+	private , err := ecdsa.GenerateKey(curve, Rand.Reader)
+	pubKey := append(PrivateKey.PublicKey.X.Bytes(),private.PublicKey.Y.Bytes()...)
+
+	return *private,pubKey
+}
