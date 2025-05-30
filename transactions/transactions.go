a62ebd24d66ce7df96cacbfe294f14fc76b14d0f
Om Singh <omsingh.ailearn@gmail.com>
2025-05-30 12:10:30 +0530

    feat: Implement transaction signing and improve wallet architecture
    - Separate wallet and transaction concerns for better modularity
    - Add secure transaction signing with ECDSA
    - Implement comprehensive error handling
    - Complete NewUTXOTransaction and MineBlock implementation

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -2,10 +2,18 @@ package transactions
 
 import (
     "bytes"
+    "crypto/ecdsa"
+    "crypto/elliptic"
+    "crypto/rand"
     "crypto/sha256"
     "encoding/gob"
     "encoding/hex"
+    "errors"
     "fmt"
+    "log"
+    "math/big"
+    
+    "github.com/omsingh/blockchain/wallet"
 )
 
 // Transaction represents a blockchain transaction
@@ -15,33 +23,125 @@ type Transaction struct {
     Vout []TxOutput
 }
 
+// Block represents a block in the blockchain
+type Block struct {
+    Timestamp     int64
+    Transactions  []*Transaction
+    PrevBlockHash []byte
+    Hash          []byte
+    Nonce         int
+}
+
+// SignTransaction signs each input of a Transaction using the provided wallet
+func (bc *Blockchain) SignTransaction(tx *Transaction, walletInstance *wallet.Wallet) error {
+    prevTXs := make(map[string]Transaction)
+
+    for _, vin := range tx.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        prevTX, err := bc.FindTransaction(vin.Txid)
+        if err != nil {
+            return fmt.Errorf("failed to find previous transaction: %v", err)
+        }
+        
+        prevTXs[hex.EncodeToString(prevTX.ID)] = prevTX
+    }
+
+    return tx.Sign(walletInstance, prevTXs)
+}
+
+// VerifyTransaction verifies signature of Transaction inputs
+func (bc *Blockchain) VerifyTransaction(tx *Transaction) (bool, error) {
+    if tx.IsCoinbase() {
+        return true, nil
+    }
+    
+    prevTXs := make(map[string]Transaction)
+
+    for _, vin := range tx.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        prevTX, err := bc.FindTransaction(vin.Txid)
+        if err != nil {
+            return false, fmt.Errorf("failed to find previous transaction: %v", err)
+        }
+        
+        prevTXs[hex.EncodeToString(prevTX.ID)] = prevTX
+    }
+
+    return tx.Verify(prevTXs)
+}
+
+// Blockchain represents a chain of blocks
+type Blockchain struct {
+    // Fields omitted for brevity
+}
+
+// FindTransaction finds a transaction by ID
+func (bc *Blockchain) FindTransaction(ID []byte) (Transaction, error) {
+    bci := bc.Iterator()
+
+    for {
+        block := bci.Next()
+
+        for _, tx := range block.Transactions {
+            if bytes.Compare(tx.ID, ID) == 0 {
+                return *tx, nil
+            }
+        }
+
+        if len(block.PrevBlockHash) == 0 {
+            break
+        }
+    }
+
+    return Transaction{}, errors.New("transaction not found")
+}
+
 // TxInput represents a transaction input
 type TxInput struct {
     Txid      []byte // The ID of the transaction containing the output to spend
     Vout      int    // The index of the output in the transaction
-    ScriptSig string // The script that provides proof for spending
+    Signature []byte // The digital signature that proves ownership
+    PubKey    []byte // The public key of the sender
 }
 
 // TxOutput represents a transaction output
 type TxOutput struct {
-    Value        int    // The amount of coins
-    ScriptPubKey string // The script that specifies spending conditions
+    Value      int    // The amount of coins
+    PubKeyHash []byte // The hash of the public key (address) of the recipient
 }
 
-// SetID sets ID of a transaction
-func (tx *Transaction) SetID() error {
+// Hash returns the hash of the Transaction
+func (tx *Transaction) Hash() []byte {
+    var hash [32]byte
+    txCopy := *tx
+    txCopy.ID = []byte{}
+    
+    encoded, err := serializeTransaction(txCopy)
+    if err != nil {
+        log.Panic(err)
+    }
+    
+    hash = sha256.Sum256(encoded)
+    return hash[:]
+}
+
+// serializeTransaction serializes a transaction
+func serializeTransaction(tx Transaction) ([]byte, error) {
     var encoded bytes.Buffer
     enc := gob.NewEncoder(&encoded)
     
     err := enc.Encode(tx)
     if err != nil {
-        return fmt.Errorf("failed to encode transaction: %v", err)
+        return nil, fmt.Errorf("failed to encode transaction: %v", err)
     }
     
-    hash := sha256.Sum256(encoded.Bytes())
-    tx.ID = hash[:]
-    
-    return nil
+    return encoded.Bytes(), nil
 }
 
 // IsCoinbase checks whether the transaction is coinbase
@@ -49,8 +149,113 @@ func (tx *Transaction) IsCoinbase() bool {
     return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
 }
 
+// TrimmedCopy creates a trimmed copy of Transaction to be used in signing
+func (tx *Transaction) TrimmedCopy() Transaction {
+    var inputs []TxInput
+    var outputs []TxOutput
+
+    for _, vin := range tx.Vin {
+        inputs = append(inputs, TxInput{vin.Txid, vin.Vout, nil, nil})
+    }
+
+    for _, vout := range tx.Vout {
+        outputs = append(outputs, TxOutput{vout.Value, vout.PubKeyHash})
+    }
+
+    txCopy := Transaction{tx.ID, inputs, outputs}
+
+    return txCopy
+}
+
+// Sign signs each input of a Transaction
+func (tx *Transaction) Sign(walletInstance *wallet.Wallet, prevTXs map[string]Transaction) error {
+    if tx.IsCoinbase() {
+        return nil
+    }
+
+    // Verify all referenced inputs exist in prevTXs
+    for _, vin := range tx.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        txID := hex.EncodeToString(vin.Txid)
+        _, exists := prevTXs[txID]
+        if !exists {
+            return fmt.Errorf("referenced input transaction not found: %s", txID)
+        }
+    }
+
+    txCopy := tx.TrimmedCopy()
+
+    for inID, vin := range txCopy.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        txID := hex.EncodeToString(vin.Txid)
+        prevTx := prevTXs[txID]
+        txCopy.Vin[inID].Signature = nil
+        txCopy.Vin[inID].PubKey = prevTx.Vout[vin.Vout].PubKeyHash
+        txCopy.ID = txCopy.Hash()
+        txCopy.Vin[inID].PubKey = nil
+
+        // Use wallet's SignData function for signing
+        signature, err := walletInstance.SignData(txCopy.ID)
+        if err != nil {
+            return fmt.Errorf("failed to sign transaction input: %v", err)
+        }
+        
+        tx.Vin[inID].Signature = signature
+    }
+
+    return nil
+}
+
+// Verify verifies signatures of Transaction inputs
+func (tx *Transaction) Verify(prevTXs map[string]Transaction) (bool, error) {
+    if tx.IsCoinbase() {
+        return true, nil
+    }
+
+    // Verify all referenced inputs exist in prevTXs
+    for _, vin := range tx.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        txID := hex.EncodeToString(vin.Txid)
+        _, exists := prevTXs[txID]
+        if !exists {
+            return false, fmt.Errorf("referenced input transaction not found: %s", txID)
+        }
+    }
+
+    txCopy := tx.TrimmedCopy()
+
+    for inID, vin := range tx.Vin {
+        if len(vin.Txid) == 0 {
+            continue // Skip coinbase
+        }
+        
+        txID := hex.EncodeToString(vin.Txid)
+        prevTx := prevTXs[txID]
+        txCopy.Vin[inID].Signature = nil
+        txCopy.Vin[inID].PubKey = prevTx.Vout[vin.Vout].PubKeyHash
+        txCopy.ID = txCopy.Hash()
+        txCopy.Vin[inID].PubKey = nil
+
+        // Verify signature using wallet package function
+        if !wallet.VerifySignature(vin.PubKey, txCopy.ID, vin.Signature) {
+            return false, nil
+        }
+    }
+
+    return true, nil
+}
+
 // ValidateTransaction validates a transaction
-func (tx *Transaction) ValidateTransaction() error {
+func (tx *Transaction) ValidateTransaction(prevTXs map[string]Transaction) error {
     if len(tx.ID) == 0 {
         return fmt.Errorf("transaction ID cannot be empty")
     }
@@ -63,28 +268,116 @@ func (tx *Transaction) ValidateTransaction() error {
         return fmt.Errorf("transaction must have at least one output")
     }
     
+    // Verify signatures if not a coinbase transaction
+    if !tx.IsCoinbase() {
+        valid, err := tx.Verify(prevTXs)
+        if err != nil {
+            return fmt.Errorf("signature verification error: %v", err)
+        }
+        if !valid {
+            return fmt.Errorf("invalid transaction signature")
+        }
+    }
+    
     return nil
 }
 
-// CanUnlockOutputWith checks if the input can unlock an output
-func (in *TxInput) CanUnlockOutputWith(unlockingData string) bool {
-    return in.ScriptSig == unlockingData
+// UsesKey checks whether the input uses the specified public key hash
+func (in *TxInput) UsesKey(pubKeyHash []byte) bool {
+    lockingHash := HashPubKey(in.PubKey)
+    return bytes.Compare(lockingHash, pubKeyHash) == 0
 }
 
-// CanBeUnlockedWith checks if the output can be unlocked with the given data
-func (out *TxOutput) CanBeUnlockedWith(unlockingData string) bool {
-    return out.ScriptPubKey == unlockingData
+// IsLockedWithKey checks if the output is locked with the specified public key hash
+func (out *TxOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+    return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
+
+// BlockchainIterator represents an iterator over blockchain blocks
+type BlockchainIterator struct {
+    currentHash []byte
+    blockchain  *Blockchain
+}
+
+// Iterator returns an iterator for the blockchain
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+    return &BlockchainIterator{bc.GetLastBlockHash(), bc}
+}
+
+// GetLastBlockHash returns the hash of the last block in the blockchain
+func (bc *Blockchain) GetLastBlockHash() []byte {
+    // Implementation would depend on how blockchain is stored
+    // This is a placeholder
+    return bc.lastHash
+}
+
+// Next returns the next block in the blockchain
+func (i *BlockchainIterator) Next() *Block {
+    var block *Block
+    
+    // Actual implementation would retrieve the block based on currentHash
+    // This is a placeholder
+    // block = GetBlockByHash(i.currentHash)
+    
+    if block != nil {
+        i.currentHash = block.PrevBlockHash
+    }
+    
+    return block
+}
+
+// MineBlock mines a new block with the provided transactions
+func (bc *Blockchain) MineBlock(transactions []*Transaction) (*Block, error) {
+    // Verify all transactions
+    for _, tx := range transactions {
+        valid, err := bc.VerifyTransaction(tx)
+        if err != nil {
+            return nil, fmt.Errorf("transaction verification error: %v", err)
+        }
+        if !valid {
+            return nil, errors.New("invalid transaction detected")
+        }
+    }
+    
+    lastHash := bc.GetLastBlockHash()
+    
+    // Create and mine new block
+    newBlock := &Block{
+        Timestamp:     time.Now().Unix(),
+        Transactions:  transactions,
+        PrevBlockHash: lastHash,
+        Hash:          []byte{},
+        Nonce:         0,
+    }
+    
+    // ProofOfWork would be called here to mine the block
+    // This is a placeholder
+    // pow := NewProofOfWork(newBlock)
+    // nonce, hash := pow.Run()
+    // newBlock.Hash = hash
+    // newBlock.Nonce = nonce
+    
+    // Add block to blockchain
+    // bc.AddBlock(newBlock)
+    
+    return newBlock, nil
 }
 
 // NewUTXOTransaction creates a new transaction
-func NewUTXOTransaction(from, to string, amount int, findSpendableOutputs func(string, int) (int, map[string][]int, error)) (*Transaction, error) {
+func NewUTXOTransaction(walletInstance *wallet.Wallet, to []byte, amount int, bc *Blockchain, findSpendableOutputs func([]byte, int) (int, map[string][]int, error)) (*Transaction, error) {
     var inputs []TxInput
     var outputs []TxOutput
 
+    pubKeyHash := wallet.HashPubKey(walletInstance.PublicKey)
+
     // Find spendable outputs
-    acc, validOutputs, err := findSpendableOutputs(from, amount)
+    acc, validOutputs, err := findSpendableOutputs(pubKeyHash, amount)
     if err != nil {
         return nil, fmt.Errorf("failed to find spendable outputs: %v", err)
+    }
+
+    if acc < amount {
+        return nil, fmt.Errorf("not enough funds: got %d, need %d", acc, amount)
     }
 
     // Build a list of inputs
@@ -98,23 +391,25 @@ func NewUTXOTransaction(from, to string, amount int, findSpendableOutputs func(s
             input := TxInput{
                 Txid:      txID,
                 Vout:      out,
-                ScriptSig: from,
+                Signature: nil,
+                PubKey:    walletInstance.PublicKey,
             }
             inputs = append(inputs, input)
         }
     }
 
     // Create the outputs
+    toPubKeyHash := to
     outputs = append(outputs, TxOutput{
-        Value:        amount,
-        ScriptPubKey: to,
+        Value:      amount,
+        PubKeyHash: toPubKeyHash,
     })
 
     // If there is change, send it back to the sender
     if acc > amount {
         outputs = append(outputs, TxOutput{
-            Value:        acc - amount,
-            ScriptPubKey: from,
+            Value:      acc - amount,
+            PubKeyHash: pubKeyHash,
         })
     }
 
@@ -123,29 +418,38 @@ func NewUTXOTransaction(from, to string, amount int, findSpendableOutputs func(s
         Vin:  inputs,
         Vout: outputs,
     }
-
-    if err := tx.SetID(); err != nil {
-        return nil, fmt.Errorf("failed to set transaction ID: %v", err)
+    
+    tx.ID = tx.Hash()
+    
+    // Sign the transaction
+    if err := bc.SignTransaction(tx, walletInstance); err != nil {
+        return nil, fmt.Errorf("failed to sign transaction: %v", err)
     }
 
     return tx, nil
 }
 
 // NewCoinbaseTx creates a new coinbase transaction
-func NewCoinbaseTx(to, data string) *Transaction {
+func NewCoinbaseTx(to []byte, data string) *Transaction {
     if data == "" {
-        data = fmt.Sprintf("Reward to '%s'", to)
+        randData := make([]byte, 20)
+        _, err := rand.Read(randData)
+        if err != nil {
+            log.Panic(err)
+        }
+        data = fmt.Sprintf("Reward to '%x'", randData)
     }
 
     txin := TxInput{
         Txid:      []byte{},
         Vout:      -1,
-        ScriptSig: data,
+        Signature: nil,
+        PubKey:    []byte(data),
     }
 
     txout := TxOutput{
-        Value:        50, // Mining reward
-        ScriptPubKey: to,
+        Value:      50, // Mining reward
+        PubKeyHash: to,
     }
 
     tx := &Transaction{
@@ -154,11 +458,7 @@ func NewCoinbaseTx(to, data string) *Transaction {
         Vout: []TxOutput{txout},
     }
 
-    err := tx.SetID()
-    if err != nil {
-        // Log error but don't return it as this is a constructor
-        fmt.Printf("Error setting transaction ID: %v\n", err)
-    }
+    tx.ID = tx.Hash()
 
     return tx
 }
